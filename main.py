51ee95a4b7501dc69a068bc0db2ccb2c5c6ffe13
Alpana Dixit <alpanadixit3@gmail.com>
2026-07-28 14:19:22 +0530

    End of query parameters

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
 @app.get('/blog')
-def index(limit):
+def index(limit = 10, published: bool = True, sort: Optional[str]= None):
     # only get 10 published blogs
-    return {'data':f'{limit} blogs from the db'}
+    if published:
+        return {'data':f'{limit} published blogs from the db'}
+    else:
+        return {'data':f'{limit} blogs from the db'}
 
 @app.get('/blog/unplished')
 def unplished():
@@ -17,5 +21,6 @@ def show(id: int):
     return {'data': id}
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit: int = 10):
+    return limit
     return {'data': {'1','2'}}
