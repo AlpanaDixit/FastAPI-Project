05def2ef87e5a2ff7b2244d67a6c6c1f95b1b3aa
Alpana Dixit <alpanadixit3@gmail.com>
2026-07-31 21:02:50 +0530

    Status code for create

--- blog/main.py
+++ blog/main.py
@@ -16,7 +16,7 @@ def get_db():
     finally:
         db.close()
 
-@app.post('/blog')
+@app.post('/blog', status_code=201)
 def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
     new_blog = models.Blog(title = request.title, body = request.body)
@@ -25,4 +25,14 @@ def create(request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(new_blog)
     return new_blog
 
+@app.get('/blog')
+def all(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
 
+@app.get('/blog/{id}')
+def show(id, db: Session = Depends(get_db)):
+    print("db", db)
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    print(blog)
+    return blog
